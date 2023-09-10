a97d8c4006a902480219d7ebbf5f0454c9436aa0
Clemens Gogoljn <service@cylancer.net>
2023-09-10 15:54:05 +0200

    Fix the plugin configuration / registry

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,10 +1,22 @@
 <?php
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
+defined('TYPO3') or die();
 
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['cysendmails_messageform'] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-    // plugin signature: <extension key without underscores> '_' <plugin name in lowercase>
-    'cysendmails_messageform',
-    // Flexform configuration schema file
-    'FILE:EXT:cy_send_mails/Configuration/FlexForms/MessageForm.xml'
+(static function (): void{
+
+    ExtensionUtility::registerPlugin(
+        'CySendMails',
+        'MessageForm',
+        'LLL:EXT:cy_send_mails/Resources/Private/Language/locallang_be_messageForm.xlf:plugin.name'
     );
+
+    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['cysendmails_messageform'] = 'pi_flexform';
+    ExtensionManagementUtility::addPiFlexFormValue(
+        // plugin signature: <extension key without underscores> '_' <plugin name in lowercase>
+        'cysendmails_messageform',
+        // Flexform configuration schema file
+        'FILE:EXT:cy_send_mails/Configuration/FlexForms/MessageForm.xml'
+    );
+})();
